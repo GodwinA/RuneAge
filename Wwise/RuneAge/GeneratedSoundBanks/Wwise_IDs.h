9216b77a1b2fdcde293474d0e45cd98d6c8a4db7
Numbox <50031430+Numbox@users.noreply.github.com>
2020-03-19 15:17:03 +0100

    Added new "inside" and "outside" states + IsInside trigger box. Also added default "outside" state to RuneAge gamemode bp

--- Wwise/RuneAge/GeneratedSoundBanks/Wwise_IDs.h
+++ Wwise/RuneAge/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,8 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID AMBIENCE_INSIDE = 3695937480U;
+        static const AkUniqueID AMBIENCE_OUTSIDE = 3996256281U;
         static const AkUniqueID ARCANE_SPELL = 2637742288U;
         static const AkUniqueID CAST_SPELL = 4080882651U;
         static const AkUniqueID FIRE_SPELL = 509606434U;
@@ -24,6 +25,18 @@ namespace AK
 
     namespace STATES
     {
+        namespace LOCATION
+        {
+            static const AkUniqueID GROUP = 1176052424U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INSIDE = 3553349781U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OUTSIDE = 438105790U;
+            } // namespace STATE
+        } // namespace LOCATION
+
         namespace SHAPE
         {
             static const AkUniqueID GROUP = 3020443768U;
@@ -72,6 +85,8 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID AMBIENCE_INSIDE = 3695937480U;
+        static const AkUniqueID AMBIENCE_OUTSIDE = 3996256281U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID SFX = 393239870U;
